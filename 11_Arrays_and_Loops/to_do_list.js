20a361e61e675eb7202e19d6d710c19268c105c4
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-02-26 10:20:07 -0600

    Basic JS

---
+++ 11_Arrays_and_Loops/to_do_list.js
@@ -0,0 +1,18 @@
+const todoList = [];
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  // document is a global object representing the entire HTML document.
+  // querySelector('.js-name-input') is a method that is used to select the first element in the document that matches a specified CSS selector. In this case, it's looking for an element with the class name js-name-input.
+  // The selected element (presumably an input element) is then stored in the variable inputElement using the const keyword, which declares a constant variable.
+
+  // inputElement now holds a reference to the selected input element.
+  // inputElement.value retrieves the current value entered or selected in the input element. It is then assigned to the variable name.
+  // Assuming inputElement refers to an input field where a user can type their name, the name variable now contains the text entered by the user in that input field.
+
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = '';
+}
