98266448b98944341018d0b9ef378673f4380294
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-02-27 10:14:05 -0600

    Basic JS

--- 11_Arrays_and_Loops/to_do_list.js
+++ 11_Arrays_and_Loops/to_do_list.js
@@ -1,15 +1,27 @@
-const todoList = ["make dinner", "wash dishes"];
+const todoList = [{
+  name: "make dinner",
+  dueDate: '2022-12-22'},
+  { name: "wash dishes",
+    dueDate: '2022-12-22'}];
 
 renderTodoList();
 
 function renderTodoList () {
   let todoListHTML = '';
     for (let i=0; i < todoList.length; i++) {
-      const todo = todoList[i];
-      const html = `<p>${todo}</p>`;
+      const todoObject = todoList[i];
+      const name = todoObject.name;
+      const dueDate = todoObject.dueDate
+      const html = `
+      <p>
+        ${name} ${dueDate} 
+        <button onclick="
+          todoList.splice(${i},1);
+          renderTodoList();
+        ">Delete</button> 
+      </p>`;
       todoListHTML += html;
     }
-  console.log(todoListHTML);
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 function addTodo() {
@@ -23,8 +35,13 @@ function addTodo() {
   // inputElement.value retrieves the current value entered or selected in the input element. It is then assigned to the variable name.
   // Assuming inputElement refers to an input field where a user can type their name, the name variable now contains the text entered by the user in that input field.
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name: name,
+    dueDate: dueDate
+  });
 
   inputElement.value = '';
   renderTodoList();
