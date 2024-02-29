b5df408888739a2ea8f3ef238a4c28195cabfead
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-02-29 15:11:44 -0600

    Basic JS

--- practice/todo.js
+++ practice/todo.js
@@ -1,10 +1,13 @@
+// What is logic here?
+// 1) Take an input
+// 2) Store that input
+// 3) Display that input
+
 const todoList = [];
 
-function addToList () {
-  const inputTask = document.querySelector('js-name-input');
-  const name = inputTask.value;
-  todoList.push(name);
-  
-  const inputDate = document.querySelector('js-due-date-input')
-  const date = inputDate.value;
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const task = inputElement.value;
+  todoList.push(task);
+  console.log(todoList);
 }
