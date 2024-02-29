a85b4ce5c149e9a556a7a53faff02ffd7cbfc304
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-02-29 15:48:57 -0600

    Basic JS

--- practice/todo.js
+++ practice/todo.js
@@ -8,20 +8,23 @@ const todoList = [];
 
 function renderTodoList() {
   let taskList = '';
-  for (let i=0; i < todoList; i++) {
+  for (let i=0; i < todoList.length; i++) {
     const todoTask = todoList[i];
 
     const html = `
-      <p>${todoTask}</p>
+      <p>${todoTask}
+        <button>Completed</button>
+      </p>
     `
     taskList = taskList + html;
-    console.log(taskList);
   }
+  document.querySelector('.js-todo-list').innerHTML = taskList;
 }
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const task = inputElement.value;
   todoList.push(task);
+  inputElement.value = ''
   renderTodoList();
 }
